83c8813ba311f5e8b367614f5dc29486c477c6a9
Christopher Huanca <christopher.huanca>
2020-08-25 18:48:28 -0400

    Validation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
@@ -27,22 +28,19 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {   
         $input = $request->all();
         
-        $this->validate($request, [
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-
         $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
         if(auth()->attempt(array($fieldType => $input['username'], 'password' => $input['password'])))
         {
             return redirect()->route('home');
         }else{
-            return redirect()->route('login')
-                ->with('error','Email-Address And Password Are Wrong.');
+            return redirect()->back()->withErrors([
+                'username' => 'Campo incorrecto',
+                'password' => 'Campo Incorrecto',
+            ]);
         }
         
     }
