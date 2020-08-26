02456ce625b82cc3c0a26ef4c708dbf243722ea8
Christopher Huanca <christopher.huanca>
2020-08-26 14:17:41 -0400

    Seeders for users

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,6 +12,39 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+
+        $administrador = User::create([
+            'name' => 'administrador',
+            'lastname' => 'administrador',
+            'email' => 'administrador@hotmail.com',
+            'username' => 'pukis.admin',
+            'password' => bcrypt('password'),
+            'address' => 'Calle 23 de Marzo entre Adrian Patiño',
+            'phone' => '4285074'
+        ]);
+
+        $cajero = User::create([
+            'name' => 'Cajero',
+            'lastname' => 'Cajero',
+            'email' => 'cajero@hotmail.com',
+            'username' => 'pukis.cajero',
+            'password' => bcrypt('password'),
+            'address' => 'Calle 23 de Marzo',
+            'phone' => '71754190'
+        ]);
+
+        $secretaria = User::create([
+            'name' => 'secretaria',
+            'lastname' => 'secretaria',
+            'email' => 'secretaria@hotmail.com',
+            'username' => 'pukis.secretaria',
+            'password' => bcrypt('password'),
+            'address' => 'Calle 23 de Marzo entre Adrian Patiño',
+            'phone' => '4241970'
+        ]);
+
+        
+
         factory(User::class, 50)->create();
     }
 }
